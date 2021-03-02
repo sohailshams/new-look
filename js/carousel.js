4adcf8d33bbb432a6b4a31caa2e9d4b8ae10b296
sohailshams <sohailshamas@yahoo.com>
2021-03-02 22:16:08 +0100

    Created carousel

---
+++ js/carousel.js
@@ -0,0 +1,301 @@
+var productData = [{
+    productUrl: 'https://www.newlook.com/uk/womens/clothing/coats-jackets/dark-grey-faux-fur-longline-coat/p/619712403',
+    imageSrc: 'https://media2.newlookassets.com/i/newlook/619712403.jpg',
+    productTitle: 'Dark Grey Faux Fur Longline Coat',
+    price: '45.99',
+    category: 'Coats Jackets',
+},
+{
+    productUrl: 'https://www.newlook.com/uk/womens/clothing/tops/black-ribbed-long-sleeve-roll-neck-top/p/635105501',
+    imageSrc: 'https://media2.newlookassets.com/i/newlook/635105501.jpg',
+    productTitle: 'Black Ribbed Long Sleeve Roll Neck Top',
+    price: '8.99',
+    category: 'Tops',
+},
+{
+    productUrl: 'https://www.newlook.com/uk/womens/clothing/leggings/black-coated-leather-look-leggings/p/634018201',
+    imageSrc: 'https://media3.newlookassets.com/i/newlook/634018201.jpg',
+    productTitle: 'Black Coated Leather-Look Leggings',
+    price: '19.99',
+    category: 'Leggings',
+},
+{
+    productUrl: 'https://www.newlook.com/uk/womens/clothing/coats-jackets/black-faux-fur-longline-coat/p/619712401',
+    imageSrc: 'https://media3.newlookassets.com/i/newlook/619712401.jpg',
+    productTitle: 'Black Faux Fur Longline Coat',
+    price: '45.99',
+    category: 'Coats Jackets',
+},
+{
+    productUrl: 'https://www.newlook.com/uk/womens/clothing/jeans/black-high-waist-super-skinny-hallie-jeans/p/635654901',
+    imageSrc: 'https://media2.newlookassets.com/i/newlook/635654901.jpg',
+    productTitle: 'Black High Waist Super Skinny Hallie Jeans',
+    price: '25.99',
+    category: 'Jeans',
+},
+{
+    productUrl: 'https://www.newlook.com/uk/womens/clothing/coats-jackets/formal-coats/camel-revere-collar-coat/p/619653214',
+    imageSrc: 'https://media2.newlookassets.com/i/newlook/619653214.jpg',
+    productTitle: 'Camel Revere Collar Coat',
+    price: '35.99',
+    category: 'Coats Jackets',
+},
+{
+    productUrl: 'https://www.newlook.com/uk/womens/clothing/jeans/black-%27lift-%26-shape%27-jeggings/p/623140201',
+    imageSrc: 'https://media3.newlookassets.com/i/newlook/623140201.jpg',
+    productTitle: 'Black Lift & Shape Jeggings',
+    price: '17.99',
+    category: 'Jeans',
+},
+{
+    productUrl: 'https://www.newlook.com/uk/womens/clothing/coats-jackets/black-leather-look-aviator-jacket/p/632207701',
+    imageSrc: 'https://media2.newlookassets.com/i/newlook/632207701.jpg',
+    productTitle: 'Black Leather-Look Aviator Jacket',
+    price: '55.99',
+    category: 'Coats Jackets',
+},
+{
+    productUrl: 'https://www.newlook.com/uk/womens/clothing/coats-jackets/formal-coats/light-grey-fluffy-houndstooth-double-breasted-coat-/p/631197208',
+    imageSrc: 'https://media3.newlookassets.com/i/newlook/631197208.jpg',
+    productTitle: 'Light Grey Fluffy Houndstooth Double Breasted Coat',
+    price: '49.99',
+    category: 'Coats Jackets',
+},
+{
+    productUrl: 'https://www.newlook.com/uk/womens/clothing/leggings/black-high-waist-leggings/p/616323901',
+    imageSrc: 'https://media3.newlookassets.com/i/newlook/616323901.jpg',
+    productTitle: 'Black High Waist Leggings',
+    price: '8.99',
+    category: 'Leggings',
+},
+{
+    productUrl: 'https://www.newlook.com/uk/womens/clothing/jeans/black-coated-super-skinny-hallie-jeans/p/631910801',
+    imageSrc: 'https://media2.newlookassets.com/i/newlook/631910801.jpg',
+    productTitle: 'Black Coated Super Skinny Hallie Jeans',
+    price: '27.99',
+    category: 'Jeans',
+},
+{
+    productUrl: 'https://www.newlook.com/uk/womens/footwear/boots/black-leather-look-chunky-lace-up-boots/p/629029201',
+    imageSrc: 'https://media3.newlookassets.com/i/newlook/629029201.jpg',
+    productTitle: 'Black Leather-Look Chunky Lace Up Boots',
+    price: '27.99',
+    category: 'Boots',
+},
+{
+    productUrl: 'https://www.newlook.com/uk/womens/clothing/jeans/black-%27lift-%26-shape%27-high-waist-skinny-jeans/p/631911801',
+    imageSrc: 'https://media3.newlookassets.com/i/newlook/631911801.jpg',
+    productTitle: 'Black Lift & Shape High Waist Skinny Jeans',
+    price: '25.99',
+    category: 'Jeans',
+},
+{
+    productUrl: 'https://www.newlook.com/uk/womens/footwear/boots/wide-fit-black-teddy-lined-chunky-lace-up-boots/p/637716701',
+    imageSrc: 'https://media3.newlookassets.com/i/newlook/637716701.jpg',
+    productTitle: 'Wide Fit Black Teddy Lined Chunky Lace Up Boots',
+    price: '27.99',
+    category: 'Boots',
+},
+{
+    productUrl: 'https://www.newlook.com/uk/womens/clothing/coats-jackets/dark-green-faux-fur-longline-coat/p/619712438',
+    imageSrc: 'https://media3.newlookassets.com/i/newlook/619712438.jpg',
+    productTitle: 'Dark Green Faux Fur Longline Coat',
+    price: '45.99',
+    category: 'Coats Jackets',
+},
+{
+    productUrl: 'https://www.newlook.com/uk/womens/clothing/nightwear/pale-grey-fluffy-hooded-dressing-gown/p/629877202',
+    imageSrc: 'https://media3.newlookassets.com/i/newlook/629877202.jpg',
+    productTitle: 'Pale Grey Fluffy Hooded Dressing Gown',
+    price: '25.99',
+    category: 'Nightwear',
+},
+{
+    productUrl: 'https://www.newlook.com/uk/womens/clothing/coats-jackets/formal-coats/pale-grey-revere-collar-coat/p/619653202',
+    imageSrc: 'https://media2.newlookassets.com/i/newlook/619653202.jpg',
+    productTitle: 'Pale Grey Revere Collar Coat',
+    price: '35.99',
+    category: 'Coats Jackets',
+},
+{
+    productUrl: 'https://www.newlook.com/uk/womens/clothing/jeans/black-super-soft-super-skinny-india-jeans/p/623185401',
+    imageSrc: 'https://media2.newlookassets.com/i/newlook/623185401.jpg',
+    productTitle: 'Black Super Soft Super Skinny India Jeans',
+    price: '22.99',
+    category: 'Jeans',
+},
+{
+    productUrl: 'https://www.newlook.com/uk/womens/clothing/coats-jackets/black-leather-look-biker-jacket-/p/620187701',
+    imageSrc: 'https://media2.newlookassets.com/i/newlook/620187701.jpg',
+    productTitle: 'Black Leather-Look Biker Jacket',
+    price: '32.99',
+    category: 'Coats Jackets',
+},
+{
+    productUrl: 'https://www.newlook.com/uk/womens/footwear/boots/black-leather-flat-chelsea-boots/p/633518801',
+    imageSrc: 'https://media2.newlookassets.com/i/newlook/633518801.jpg',
+    productTitle: 'Black Leather Flat Chelsea Boots',
+    price: '35.99',
+    category: 'Boots',
+}];
+
+var searchedRecords = productData;
+
+var carousel = function(){
+  var loadData = function(products){
+    var html = '';
+    var initialEle = 0;
+
+    $('#myCarousel').carousel('dispose');
+
+    if(products.length == 0){
+      $('#myCarousel').addClass('d-none');
+      $('.no-record-found').removeClass('d-none');
+      return false;
+    }
+
+      $('.no-record-found').addClass('d-none');
+    $('#myCarousel').removeClass('d-none');
+
+    $.each(products, function(index, product){
+      var active = (initialEle == index) ? 'active' : '';
+
+      html += '<div class="carousel-item ' + active + '">' +
+				'<div class="col-lg-4 col-md-6 slide-container">' +
+                  '<img class="img-fluid" src="' + product.imageSrc + '" alt="' + product.productTitle + '" />' +
+                  '<div class="slide-title-container">' +
+                      '<div class="slide-title">' + product.productTitle + '</div>' +
+                      '<a class="btn" href="' + product.productUrl + '">Visit Site</a>'+
+                      '<a class="btn modal-popup" href="javascript:void(0)" data-product_id="' + index + '">View Popup</a>'+
+                  '</div>' +
+				'</div>' +
+              '</div>';
+    });
+
+    $('.carousel-inner').html(html);
+    initiateSlider();
+    initiatePopup();
+  }
+  
+  var initiateSlider = function(){
+	  $('#myCarousel').carousel({
+		interval: 2000
+	})
+
+	$('.carousel .carousel-item').each(function() {
+		var minPerSlide = 4;
+		var next = $(this).next();
+		if (!next.length) {
+			next = $(this).siblings(':first');
+		}
+		next.children(':first-child').clone().appendTo($(this));
+
+		for (var i = 0; i < minPerSlide; i++) {
+			next = next.next();
+			if (!next.length) {
+				next = $(this).siblings(':first');
+			}
+
+			next.children(':first-child').clone().appendTo($(this));
+		}
+	});
+  }
+
+  var initiatePopup = function(){
+    $(document).on("click",".modal-popup", function () {
+
+      var productIndex = $(this).data('product_id');
+
+      $('#product_detail_modal .modal-title').html(searchedRecords[productIndex].productTitle);
+      $('#product_detail_modal .product-img').attr('src', searchedRecords[productIndex].imageSrc);
+      $('#product_detail_modal').modal('show');
+    });
+  }
+
+  var loadPriceRange = function(){
+    var minPrice = maxPrice = 0;
+
+    $.each(productData,function(index, product){
+      if(product.price){
+          var price = parseFloat(product.price);
+          maxPrice = (price > maxPrice) ? price : maxPrice;
+      }
+    });
+
+    maxPrice = Math.ceil(maxPrice);
+
+    $('#min_price').html(minPrice);
+    $('#max_price').html(maxPrice);
+
+    $("#price_range").slider({
+        min  : minPrice,
+        max  : maxPrice,
+        value: [minPrice, maxPrice],
+
+    });
+  }
+
+  var loadCategory = function(){
+    var categories = [];
+    var html = '<option value="">Search By Category</option>';
+
+    $.each(productData,function(index, product){
+        if(product.category && $.inArray(product.category, categories) == -1){
+          categories.push(product.category);
+
+          html += '<option value="'+ product.category +'">'+ product.category +'</option>';
+        }
+    });
+
+    $('#product_category').html(html);
+  }
+
+  var handleEvents = function(){
+    $('.filter-ele').change(function(){
+        searchedData();
+    });
+  }
+
+  var searchedData = function () {
+    searchedRecords = productData;
+    var search = $('#search').val();
+    var category = $('#product_category').val();
+    selectedPrice = $('#price_range').data('slider').getValue();
+    var minPrice = parseFloat(selectedPrice[0]);
+    var maxPrice = parseFloat(selectedPrice[1]);
+
+    if(search){
+      searchedRecords = $.grep(searchedRecords, function(e){
+          return e.productTitle.toLowerCase().includes(search.toLowerCase());
+      });
+    }
+
+    if(category){
+      searchedRecords = $.grep(searchedRecords, function(e){
+          return e.category.toLowerCase() == category.toLowerCase();
+      });
+    }
+
+    if(minPrice || maxPrice){
+      searchedRecords = $.grep(searchedRecords, function(e){
+        return parseFloat(e.price) >= minPrice && parseFloat(e.price) <= maxPrice;
+      });
+    }
+
+    loadData(searchedRecords);
+  }
+
+  return {
+    //---- main function to initiate the module
+    init: function () {
+      loadData(productData);
+      loadPriceRange();
+      loadCategory();
+      handleEvents();
+    }
+  };
+}();
+
+jQuery(function($){
+  carousel.init();
+});
